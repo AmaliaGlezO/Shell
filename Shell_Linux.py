8dff44504123128c4ac5f564e9bab326b703a5a9
AmaliaGlezO <amalia.glezo@gmail.com>
2025-04-05 23:20:46 -0400

    reindexar historial

--- Shell_Linux.py
+++ Shell_Linux.py
@@ -1,3 +1,14 @@
 import os
 import re
 import subprocess
+
+historial = {}
+# Lista de trabajos en segundo plano
+trabajos = []
+contador_trabajos = 1  # Identificador incremental para trabajos
+
+def reindexar_historial():
+    """Reindexa el historial para que las claves sean consecutivas a partir del 1."""
+    global historial
+    comandos = list(historial.values())
+    historial = {i+1: cmd for i, cmd in enumerate(comandos)}
